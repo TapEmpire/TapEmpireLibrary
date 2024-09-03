2072a8920a715e7bb6333c3bb544b307e41100b7
NorthernShady <ecandentia@gmail.com>
2024-09-03 19:49:06 +0300

    Added SceneContextsService.

---
+++ Runtime/Services/SceneContexts/ISceneContextsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Zenject;
+
+namespace TapEmpire.Services
+{
+    public interface ISceneContextsService : IService
+    {
+        event Action<string, SceneContext> OnSceneContextInstalled; 
+        
+        void AddInstalledSceneContext(string id, SceneContext sceneContext);
+    }
+}
---
+++ Runtime/Services/SceneContexts/SceneContextsService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Zenject;
+
+namespace TapEmpire.Services
+{
+    [Serializable]
+    public class SceneContextsService : Initializable, ISceneContextsService
+    {
+        private Dictionary<string, SceneContext> _sceneContexts = new ();
+        
+        public event Action<string, SceneContext> OnSceneContextInstalled;
+
+        protected override UniTask OnInitializeAsync(CancellationToken cancellationToken)
+        {
+            _sceneContexts = new Dictionary<string, SceneContext>();
+            return base.OnInitializeAsync(cancellationToken);
+        }
+
+        protected override void OnRelease()
+        {
+            _sceneContexts.Clear();
+            base.OnRelease();
+        }
+
+        public void AddInstalledSceneContext(string id, SceneContext sceneContext)
+        {
+            if (_sceneContexts.ContainsKey(id))
+            {
+                _sceneContexts.Remove(id);
+            }
+            _sceneContexts.Add(id, sceneContext);
+            OnSceneContextInstalled?.Invoke(id, sceneContext);
+        }
+    }
+}
