e07697dbe742eead6778174805d74bb728735ee0
NorthernShady <ecandentia@gmail.com>
2024-09-05 00:20:09 +0300

    Added SerializableReferencesDictionary.

---
+++ Runtime/Utility/Serialization/SerializableReferencesDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TapEmpire.Utility
+{
+    [Serializable]
+    public sealed class SerializableReferencesDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField]
+        private List<TKey> _keys = new List<TKey>();
+
+        [SerializeReference]
+        private List<TValue> _values = new List<TValue>();
+
+        public void OnAfterDeserialize()
+        {
+            Clear();
+
+            if (_keys.Count != _values.Count)
+                throw new InvalidOperationException($"There are {_keys.Count} keys and {_values.Count} values after deserialization. Make sure that both key and value types are serializable.");
+
+            for (int i = 0; i < _keys.Count; i++)
+            {
+                this[_keys[i]] = _values[i];
+            }
+        }
+
+        public void OnBeforeSerialize()
+        {
+            _keys.Clear();
+            _values.Clear();
+
+            foreach (var kvp in this)
+            {
+                _keys.Add(kvp.Key);
+                _values.Add(kvp.Value);
+            }
+        }
+    }
+
+}
