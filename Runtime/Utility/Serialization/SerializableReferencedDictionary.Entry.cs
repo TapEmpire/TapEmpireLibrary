0dd59057f931211c857bbce485092652929c2cb1
Nikita Serebriakov <whaledevelop@gmail.com>
2024-09-05 20:49:35 +0400

    SerializableReferencesDictionary in the same style as SerializableDictionary (for utility)

---
+++ Runtime/Utility/Serialization/SerializableReferencedDictionary.Entry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace TapEmpire.Utility
+{
+    public sealed partial class SerializableReferencesDictionary<TKey, TValue>
+    {
+        [Serializable]
+        private struct Entry
+        {
+            [SerializeField]
+            private TKey _key;
+
+            [SerializeReference]
+            private TValue _value;
+
+            public TKey Key
+            {
+                get => _key;
+                set => _key = value;
+            }
+
+            public TValue Value
+            {
+                get => _value;
+                set => _value = value;
+            }
+        }
+    }
+}
