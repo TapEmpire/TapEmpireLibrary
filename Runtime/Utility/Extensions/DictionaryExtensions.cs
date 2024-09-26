51470023b42c0a8536925079cf74e3af62d20c76
NorthernShady <ecandentia@gmail.com>
2024-09-26 21:31:16 +0300

    Added new extensions.

--- Runtime/Utility/Extensions/DictionaryExtensions.cs
+++ Runtime/Utility/Extensions/DictionaryExtensions.cs
@@ -26,6 +26,14 @@ namespace TapEmpire.Utility
                 return keyValueTuple;
             }
         }
+
+        public static void AddIfNone<TKey, TValue>(this Dictionary<TKey, TValue> self, TKey key, TValue value)
+        {
+            if (!self.ContainsKey(key))
+            {
+                self.Add(key, value);
+            }
+        }
         
         public static void RemoveAll<TKey, TValue>(this Dictionary<TKey, TValue> self, Func<TValue, bool> conditionToRemove)
         {
