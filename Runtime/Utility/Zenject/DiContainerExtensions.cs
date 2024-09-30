3e588e3e254c1391746ba41180e6cb740c346bc5
Nikita Serebriakov <whaledevelop@gmail.com>
2024-09-30 07:00:50 +0400

    Crash fix try in DiContainerExtensions

--- Runtime/Utility/Zenject/DiContainerExtensions.cs
+++ Runtime/Utility/Zenject/DiContainerExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Zenject;
+﻿using System;
+using UnityEngine;
+using Zenject;
 
 namespace TapEmpire.Utility
 {
@@ -8,6 +10,14 @@ namespace TapEmpire.Utility
         {
             var tickableManager = diContainer.Resolve<TickableManager>();
             foreach (var target in targets)
+            {
+                InitializeTicks(tickableManager, target);
+            }
+        }
+        
+        public static void InitializeTicks<T>(this TickableManager tickableManager, T target) where T : class
+        {
+            try
             {
                 if (target is ITickable tickable)
                 {
@@ -22,6 +32,15 @@ namespace TapEmpire.Utility
                     tickableManager.AddLate(lateTickable);
                 }
             }
+            // я сам не люблю try catch, но тут ошибка внутри zenject. Он видимо внутри создает таски в разных потоках с приоритетами.
+            // и если быстро нажимать рестарт (удалять-добавлять модули), то выдает ошибку дублирования. не вижу как это можно обойти
+            // без того чтобы лезть в код zenject дальше
+            
+            // ! причем с этой ошибкой в работе ничего не ломается, игра норм играется !
+            catch (Exception e)
+            {
+                Debug.LogWarning($"InitializeTicks error {e.Message}");
+            }
         }
         
         public static void ReleaseTicks<T>(this DiContainer diContainer, params T[] targets) where T : class
@@ -30,18 +49,32 @@ namespace TapEmpire.Utility
 
             foreach (var target in targets)
             {
+                ReleaseTicks(tickableManager, target);
+            }
+        }
+        
+        public static void ReleaseTicks<T>(this TickableManager tickableManager, T target) where T : class
+        {
+            try
+            {
                 if (target is ITickable tickable)
                 {
                     tickableManager.Remove(tickable);
                 }
+
                 if (target is IFixedTickable fixedTickable)
                 {
                     tickableManager.RemoveFixed(fixedTickable);
                 }
+
                 if (target is ILateTickable lateTickable)
                 {
                     tickableManager.RemoveLate(lateTickable);
                 }
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning($"ReleaseTicks error {e.Message}");
             }
         }
     }
