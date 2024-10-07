544ac9b7160659c3af494561f0c284d951d98e6a
Nikita Serebriakov <whaledevelop@gmail.com>
2024-10-07 12:26:03 +0400

    levels patterns

--- Runtime/Services/AdsService/AdsSettings.cs
+++ Runtime/Services/AdsService/AdsSettings.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using RagDoll.Level;
 using Sirenix.OdinInspector;
+using TapEmpireLibrary.Utility;
 using UnityEngine;
 
 namespace TapEmpire.Services
@@ -14,13 +15,7 @@ namespace TapEmpire.Services
 
         [Space(5)]
         public List<int> InterstitialAfterLevels = new();
-
-        public enum LevelPattern
-        {
-            Pattern_4_8_11_Plus3,
-            Pattern_Each5
-        }
-
+        
         [Button]
         public void Clear()
         {
@@ -28,50 +23,11 @@ namespace TapEmpire.Services
         }
         
         [Button]
-        public void AddLevelsBasedOnPattern(LevelPattern pattern, LevelsTable levelsTable)
+        public void AddLevelsBasedOnPattern(LevelsPattern pattern, LevelsTable levelsTable)
         {
-            List<int> levelIndexes = new();
-
-            switch (pattern)
-            {
-                case LevelPattern.Pattern_4_8_11_Plus3:
-                    levelIndexes = GeneratePattern_4_8_11_Plus3(levelsTable.UnsortedLevels.Length);
-                    break;
-                case LevelPattern.Pattern_Each5:
-                    levelIndexes = GeneratePattern_Each5(levelsTable.UnsortedLevels.Length);
-                    break;
-            }
-
+            var levelIndexes = pattern.GetLevelsIndexes(levelsTable.UnsortedLevels.Length);
             InterstitialAfterLevels.AddRange(levelIndexes);
             InterstitialAfterLevels = RemoveDuplicatesAndSort(InterstitialAfterLevels);
-        }
-
-        private List<int> GeneratePattern_4_8_11_Plus3(int totalLevels)
-        {
-            var indexes = new List<int> { 4, 8, 11 };
-            var nextValue = 11;
-
-            while (nextValue + 3 < totalLevels)
-            {
-                nextValue += 3;
-                indexes.Add(nextValue);
-            }
-
-            return indexes.Where(index => index < totalLevels).ToList();
-        }
-
-        private List<int> GeneratePattern_Each5(int totalLevels)
-        {
-            var indexes = new List<int>();
-            var nextValue = 5;
-
-            while (nextValue < totalLevels)
-            {
-                indexes.Add(nextValue);
-                nextValue += 5;
-            }
-
-            return indexes;
         }
 
         private List<int> RemoveDuplicatesAndSort(List<int> list)
---
+++ Runtime/Utility/LevelsPattern/LevelsPatternExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TapEmpireLibrary.Utility
+{
+    public static class LevelsPatternExtensions
+    {
+        public static List<int> GetLevelsIndexes(this LevelsPattern pattern, int totalLevels)
+        {
+            return pattern switch
+            {
+                LevelsPattern.Pattern_4_8_11_Plus3 => GeneratePattern_4_8_11_Plus3(totalLevels),
+                LevelsPattern.Pattern_Each5 => GeneratePattern_Each(totalLevels, 5),
+                LevelsPattern.Pattern_Each10 => GeneratePattern_Each(totalLevels, 10),
+                _ => throw new ArgumentOutOfRangeException(nameof(pattern), pattern, null)
+            };
+        }
+        
+        private static List<int> GeneratePattern_4_8_11_Plus3(int totalLevels)
+        {
+            var indexes = new List<int> { 4, 8, 11 };
+            var nextValue = 11;
+
+            while (nextValue + 3 < totalLevels)
+            {
+                nextValue += 3;
+                indexes.Add(nextValue);
+            }
+
+            return indexes.Where(index => index < totalLevels).ToList();
+        }
+
+        private static List<int> GeneratePattern_Each(int totalLevels, int each)
+        {
+            var indexes = new List<int>();
+            var nextValue = each;
+
+            while (nextValue < totalLevels)
+            {
+                indexes.Add(nextValue);
+                nextValue += each;
+            }
+
+            return indexes;
+        }
+    }
+}
