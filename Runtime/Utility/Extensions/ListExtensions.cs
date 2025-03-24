f330bbf7899fc11da50848be54921f65553a013b
Alex Mironov <zettaiingamono@gmail.com>
2025-03-24 19:48:21 +0300

    + List shuffle

--- Runtime/Utility/Extensions/ListExtensions.cs
+++ Runtime/Utility/Extensions/ListExtensions.cs
@@ -139,5 +139,18 @@ namespace TapEmpire.Utility
         {
             return list[UnityEngine.Random.Range(0, list.Count - 1)];
         }
+        
+                
+        public static void Shuffle<T>(this IList<T> list)  
+        {  
+            int n = list.Count;  
+            while (n > 1) {  
+                n--;  
+                int k = UnityEngine.Random.Range(0, n + 1);  
+                T value = list[k];  
+                list[k] = list[n];  
+                list[n] = value;  
+            }  
+        }
     }
 }
