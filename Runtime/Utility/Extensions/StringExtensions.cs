b37d9c9a84d7b632f4c270254223926d62f29e71
NorthernShady <ecandentia@gmail.com>
2025-04-02 17:19:34 +0300

    Minor changes.

--- Runtime/Utility/Extensions/StringExtensions.cs
+++ Runtime/Utility/Extensions/StringExtensions.cs
@@ -7,5 +7,10 @@ namespace TapEmpire.Utility
             int lastIndex = str.LastIndexOf(substring);
             return lastIndex != -1 ? str.Remove(lastIndex, substring.Length) : str;
         }
+
+        public static string Escape(this string str)
+        {
+             return System.Text.RegularExpressions.Regex.Escape(str);
+        }
     }
 }
