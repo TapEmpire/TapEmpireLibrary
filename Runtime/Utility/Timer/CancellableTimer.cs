586e7007c39d487c4d629c8b995715b8927c70b6
NorthernShady <ecandentia@gmail.com>
2025-04-16 12:19:39 +0300

    Added new timers.

---
+++ Runtime/Utility/Timer/CancellableTimer.cs
@@ -0,0 +1,60 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
+using R3;
+
+namespace TapEmpire.Utility
+{
+    public class CancellableTimer : IDisposable
+    {
+        public Subject<Unit> OnDone = new();
+
+        private float _delay = 0.0f;
+        private CancellationTokenSource _tokenSource;
+
+        public CancellableTimer(float delay, Action callback)
+        {
+            _delay = delay;
+
+            if (callback != null)
+            {
+                OnDone.Subscribe(_ => callback.Invoke());
+            }
+        }
+
+        public void Restart()
+        {
+            Cancel();
+            _tokenSource?.Dispose();
+            _tokenSource = new();
+
+            Run().Forget();
+        }
+
+        public void Cancel()
+        {
+            _tokenSource?.Cancel();
+        }
+
+        public void Dispose()
+        {
+            Cancel();
+            _tokenSource?.Dispose();
+            _tokenSource = null;
+
+            OnDone.Dispose();
+        }
+
+        private async UniTask Run()
+        {
+            try
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(_delay), cancellationToken: _tokenSource.Token);
+                OnDone.OnNext(Unit.Default);
+            }
+            finally
+            {
+            }
+        }
+    }
+}
