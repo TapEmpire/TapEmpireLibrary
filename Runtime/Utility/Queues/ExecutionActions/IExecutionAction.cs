cdcfe5a258cf79588454ae5e7764f47be7d82247
NorthernShady <ecandentia@gmail.com>
2025-05-07 14:32:27 +0300

    Added result to ExecutionAction.

--- Runtime/Utility/Queues/ExecutionActions/IExecutionAction.cs
+++ Runtime/Utility/Queues/ExecutionActions/IExecutionAction.cs
@@ -3,29 +3,36 @@ using R3;
 
 namespace TapEmpire.Utility
 {
+    public enum ExecutionState
+    {
+        NotDone,
+        False,
+        True
+    }
+
     public interface IExecutionAction<T> : IDisposable where T : Enum
     {
-        ReactiveProperty<bool> OnDone { get; }
+        ReactiveProperty<ExecutionState> OnDone { get; }
 
         void Reinitialize();
-        void Execute(T flow = default);
+        void Execute(ExecutionState state, T flow = default);
         IExecutionAction<T> AddCallbackOnce(System.Action callback);
     }
 
     public class ExecutionAction<T> : IExecutionAction<T> where T : Enum
     {
-        public ReactiveProperty<bool> OnDone { get; private set; } = new(false);
+        public ReactiveProperty<ExecutionState> OnDone { get; private set; } = new(ExecutionState.NotDone);
 
         private System.Action _callback = null;
 
-        public virtual void Execute(T flow = default)
+        public virtual void Execute(ExecutionState state = default, T flow = default)
         {
             MarkComplete();
         }
 
         public virtual void Reinitialize()
         {
-            OnDone.Value = false;
+            OnDone.Value = ExecutionState.NotDone;
         }
 
         public IExecutionAction<T> AddCallbackOnce(System.Action callback)
@@ -36,9 +43,14 @@ namespace TapEmpire.Utility
 
         public virtual void Dispose() { }
 
-        protected virtual void MarkComplete()
+        protected void MarkComplete()
         {
-            OnDone.Value = true;
+            MarkComplete(true);
+        }
+
+        protected virtual void MarkComplete(bool result)
+        {
+            OnDone.Value = result ? ExecutionState.True : ExecutionState.False;
             _callback?.Invoke();
             _callback = null;
         }
