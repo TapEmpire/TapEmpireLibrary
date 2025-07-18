74e63f610e7001738b38a50db32ccb5a7705d769
NorthernShady <ecandentia@gmail.com>
2025-07-18 12:19:59 +0400

    LayoutGroupExtensions.

---
+++ Runtime/Utility/Extensions/LayoutGroupExtensions.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TapEmpire.Utility
+{
+    public static class LayoutGroupExtensions
+    {
+        public static float CalculateHeightAsGrid(this LayoutGroup self, int elementsCount)
+        {
+            var grid = self as GridLayoutGroup;
+            RectOffset padding = grid.padding;
+            float cellHeight = grid.cellSize.y;
+            float spacingY = grid.spacing.y;
+            int childCount = elementsCount; // grid.transform.childCount;
+
+            int columns = 1;
+
+            switch (grid.constraint)
+            {
+                case GridLayoutGroup.Constraint.FixedColumnCount:
+                    columns = grid.constraintCount;
+                    break;
+                case GridLayoutGroup.Constraint.FixedRowCount:
+                    columns = Mathf.CeilToInt((float)childCount / grid.constraintCount);
+                    break;
+                case GridLayoutGroup.Constraint.Flexible:
+                    Debug.LogWarning("GridLayoutGroup.Constraint.Flexible is not supported for manual size calculation.");
+                    return 0;
+            }
+
+            int rows = Mathf.CeilToInt((float)childCount / columns);
+
+            float totalHeight = padding.top + (cellHeight * rows) + (spacingY * (rows - 1)) + padding.bottom;
+            return totalHeight;
+        }
+
+        public static float CalculateHeightAsVertical(this LayoutGroup self, int elementsCount)
+        {
+            var layoutGroup = self as VerticalLayoutGroup;
+            RectOffset padding = layoutGroup.padding;
+            float spacing = layoutGroup.spacing;
+
+            float totalHeight = padding.top + padding.bottom;
+            int activeChildCount = 0;
+
+            foreach (RectTransform child in layoutGroup.transform)
+            {
+                if (!child.gameObject.activeInHierarchy)
+                    continue;
+
+                LayoutElement le = child.GetComponent<LayoutElement>();
+                float preferredHeight = le != null && le.ignoreLayout == false && le.preferredHeight >= 0
+                    ? le.preferredHeight
+                    : LayoutUtility.GetPreferredHeight(child);
+
+                totalHeight += preferredHeight;
+                activeChildCount++;
+            }
+
+            if (activeChildCount > 1)
+                totalHeight += spacing * (activeChildCount - 1);
+
+            return totalHeight;
+        }
+    }
+}
