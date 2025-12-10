2715aa0b32912581d79b07a4e914d434339682d5
Alex Mironov <zettaiingamono@gmail.com>
2025-12-10 20:02:04 +0300

    + Visual progress

--- Runtime/Services/OfferService/Analytics/OfferAnalyticsModule.cs
+++ Runtime/Services/OfferService/Analytics/OfferAnalyticsModule.cs
@@ -34,7 +34,7 @@ namespace TapEmpire.Services.Offer
 
         private void OnOfferShown((OfferType OfferType, bool Autoshown, string Placement) data)
         {
-            var level = _progressService.GetLevelProgress() + 1;
+            var level = _progressService.GetVisualProgress();
 
             _analyticsService.LogEvent(CoreAnalyticsStrings.CommonData, new Dictionary<string, object>()
             {
