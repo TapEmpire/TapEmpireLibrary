b7611875b1c305031fb0bc8fa5985424f36c5403
evgeny.mandrykin <evgenymandrykin@gmail.com>
2026-04-02 21:12:59 +0700

    live ops

---
+++ Runtime/Features/LiveOps/Data/ILiveOpsParams.cs
@@ -0,0 +1,7 @@
+namespace TapEmpire.Services.LiveOps
+{
+    public interface ILiveOpsParams
+    {
+        string Id { get; }
+    }
+}
--- Runtime/Features/LiveOps/Data/LiveOpsData.cs
+++ Runtime/Features/LiveOps/Data/LiveOpsData.cs
@@ -12,9 +12,18 @@ namespace TapEmpire.Services.LiveOps
         [SerializeReference] public List<ICondition> Conditions = new();
         public LiveOpsIcon IconPrefab;
         public LiveOpsView LiveOpsPrefab;
-        public TutorialUIView TutorialPrefab;
-
+        public TutorialUIView TutorialPrefab; 
+        public abstract string Name { get; }
         public abstract ILiveOps Create();
+        
+        //todo: possible future implementation 
+        // public abstract IReadOnlyList<ILiveOpsParams> AllParams { get; }
+        // public abstract ILiveOpsParams ActiveParams { get; }
+        // public abstract T GetLiveOpsParams<T>() where T : ILiveOpsParams;
+        // public abstract void SetActiveParamsById(string id);
+        // public abstract void SetActiveParamsByIndex(int index);
+        // public abstract ILiveOpsParams DeserializeParams(JToken token);
+        // public abstract void AddOrReplaceParams(ILiveOpsParams p);
     }
 
     public class StateData
