7900621ad40fcbe2709232210dde4c25c71c532b
NorthernShady <ecandentia@gmail.com>
2026-04-11 02:05:28 +0400

    Add token substitution list to RemoveInfoLocalizationConverter.

--- Editor/GoogleSheets/Converters/RemoveInfoLocalizationConverter.cs
+++ Editor/GoogleSheets/Converters/RemoveInfoLocalizationConverter.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using TapEmpire.Utility.GoogleSheet;
 using System.Text;
 using System.Text.RegularExpressions;
+using UnityEngine;
 
 namespace TapEmpire.Editor
 {
     [Serializable]
+    public class TokenSubstitution
+    {
+        public string Token;
+        public string Substitute;
+    }
+
+    [Serializable]
     public class RemoveInfoLocalizationConverter : ILocalizationConverter
     {
+        [SerializeField] private List<TokenSubstitution> _substitutions = new();
+
         public List<List<string>> Convert(string tableName, List<List<string>> data)
         {
             return data;
@@ -17,7 +27,17 @@ namespace TapEmpire.Editor
         public List<TranslationEntryContainer> Deconvert(string tableName, List<TranslationEntryContainer> data, StringBuilder stringBuilder)
         {
             data.ForEach(container =>
-                container.Entries.ForEach(entry => entry.LocalizedString = Regex.Replace(entry.LocalizedString, @"\s*\([^()]*\)\s*", " ").Trim()));
+                container.Entries.ForEach(entry =>
+                {
+                    entry.LocalizedString = Regex.Replace(entry.LocalizedString, @"\s*\([^()]*\)\s*", " ");
+
+                    foreach (var sub in _substitutions)
+                    {
+                        entry.LocalizedString = Regex.Replace(entry.LocalizedString, Regex.Escape(sub.Token), sub.Substitute);
+                    }
+
+                    entry.LocalizedString = entry.LocalizedString.Trim();
+                }));
             return data;
         }
     }
