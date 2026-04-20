b5a263565f06a632edc3e4813608c6ac63b86bd9
NorthernShady <ecandentia@gmail.com>
2026-04-21 00:21:41 +0400

    Simplify LiveOpsAnalyticsModule: 3 virtual On* + 3 Log* helpers, explicit LiveOpsTimeUnit param

--- Runtime/Features/LiveOps/Analytics/AnalyticsModule.LiveOps.cs
+++ Runtime/Features/LiveOps/Analytics/AnalyticsModule.LiveOps.cs
@@ -7,17 +7,25 @@ using Zenject;
 
 namespace TapEmpire.Services.LiveOps
 {
+    public enum LiveOpsTimeUnit
+    {
+        Minutes,
+        Days,
+    }
+
     public class LiveOpsAnalyticsModule<T> : IDisposable where T : ILiveOps
     {
         protected readonly T _liveOps;
         protected readonly CompositeDisposable _disposables = new();
 
         private readonly IAnalyticsService _analyticsService;
+        private readonly LiveOpsTimeUnit _timeUnit;
 
-        public LiveOpsAnalyticsModule(DiContainer diContainer, T liveOps)
+        public LiveOpsAnalyticsModule(DiContainer diContainer, T liveOps, LiveOpsTimeUnit timeUnit)
         {
             _analyticsService = diContainer.Resolve<IAnalyticsService>();
             _liveOps = liveOps;
+            _timeUnit = timeUnit;
 
             liveOps.OnStarted.Subscribe(OnStarted).AddTo(_disposables);
             liveOps.OnStage.Subscribe(OnStage).AddTo(_disposables);
@@ -29,47 +37,47 @@ namespace TapEmpire.Services.LiveOps
             _disposables.Dispose();
         }
 
-        protected virtual void OnStarted(ILiveOps liveOps)
+        protected virtual void OnStarted(ILiveOps liveOps) => LogStart(liveOps);
+        protected virtual void OnStage(ILiveOps liveOps) => LogStage(liveOps);
+        protected virtual void OnFinished(ILiveOps liveOps) => LogFinish(liveOps);
+
+        protected void LogStart(ILiveOps liveOps, params JProperty[] extra)
         {
-            LogStart(liveOps.StateData.Id);
+            LogEvent("Start", new JObject(new JProperty("Id", liveOps.StateData.Id), extra));
         }
 
-        protected virtual void OnStage(ILiveOps liveOps)
+        protected void LogStage(ILiveOps liveOps, params JProperty[] extra)
         {
-            var completeTime = (int)(DateTime.UtcNow - liveOps.StateData.StartedAt).TotalMinutes;
-            LogStage(liveOps.StateData.Id, liveOps.StateData.Inner, completeTime);
-        }
-
-        protected virtual void OnFinished(ILiveOps liveOps)
-        {
-            var completeTime = (int)(DateTime.UtcNow - liveOps.StateData.StartedAt).TotalMinutes;
-            LogFinish(liveOps.StateData.Id, liveOps.StateData.Inner, completeTime);
-        }
-
-        protected void LogStart(string id, params JProperty[] extra)
-        {
-            LogEvent("Start", new JObject(new JProperty("Id", id), extra));
-        }
-
-        protected void LogStage(string id, int stage, int completeTime, params JProperty[] extra)
-        {
+            var completeTime = GetCompleteTime(liveOps);
             LogEvent("Stage", new JObject(
-                new JProperty("Id", id),
-                new JProperty("Stage", stage),
+                new JProperty("Id", liveOps.StateData.Id),
+                new JProperty("Stage", liveOps.StateData.Inner),
                 new JProperty("CompleteTime", completeTime),
                 extra));
         }
 
-        protected void LogFinish(string id, int stage, int completeTime, params JProperty[] extra)
+        protected void LogFinish(ILiveOps liveOps, params JProperty[] extra)
         {
+            var completeTime = GetCompleteTime(liveOps);
             LogEvent("End", new JObject(
-                new JProperty("Id", id),
-                new JProperty("Stage", stage),
+                new JProperty("Id", liveOps.StateData.Id),
+                new JProperty("Stage", liveOps.StateData.Inner),
                 new JProperty("CompleteTime", completeTime),
                 extra));
         }
 
-        protected void LogEvent(string eventType, JObject properties)
+        private int GetCompleteTime(ILiveOps liveOps)
+        {
+            var elapsed = DateTime.UtcNow - liveOps.StateData.StartedAt;
+            return _timeUnit switch
+            {
+                LiveOpsTimeUnit.Minutes => (int)elapsed.TotalMinutes,
+                LiveOpsTimeUnit.Days => (int)elapsed.TotalDays,
+                _ => throw new ArgumentOutOfRangeException(nameof(_timeUnit), _timeUnit, null),
+            };
+        }
+
+        private void LogEvent(string eventType, JObject properties)
         {
             _analyticsService.LogEvent(CoreAnalyticsStrings.CommonData, new Dictionary<string, object>
             {
