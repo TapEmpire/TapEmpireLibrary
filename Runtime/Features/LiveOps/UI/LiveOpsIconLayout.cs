267dcb5fbec2bf22a2e00ac143fc7336b008e3f1
NorthernShady <ecandentia@gmail.com>
2026-05-06 23:53:23 +0500

    Add OfferButton positioning with flag-based icon tracking to LiveOpsIconLayout

--- Runtime/Features/LiveOps/UI/LiveOpsIconLayout.cs
+++ Runtime/Features/LiveOps/UI/LiveOpsIconLayout.cs
@@ -12,30 +12,57 @@ namespace TapEmpire.LiveOps.UI
 
         private readonly List<LiveOpsIcon> _generalIcons = new();
         private readonly CompositeDisposable _disposables = new();
+        private Transform _offerButton;
+        private bool _hasIconBeforeOffer = false;
 
         public void Add(LiveOpsIcon icon)
         {
-            var index = GetPlacementIndex(icon);
+            var index = GetPlacementIndex(icon.Name);
             if (index >= _firstCommonIndex)
                 _generalIcons.Add(icon);
             icon.transform.SetParent(_placements[index], false);
             icon.OnFinished.Subscribe(_ => Remove(icon)).AddTo(_disposables);
+
+            if (index == PreOfferButtonIndex)
+                _hasIconBeforeOffer = true;
+
+            RefreshOfferButton();
         }
 
         public void Remove(LiveOpsIcon icon)
         {
-            icon.gameObject.SetActive(false);
+            Destroy(icon.gameObject);
 
             if (!_generalIcons.Remove(icon))
+            {
+                if (GetPlacementIndex(icon.Name) == PreOfferButtonIndex)
+                    _hasIconBeforeOffer = false;
+                RefreshOfferButton();
                 return;
+            }
 
             for (var i = 0; i < _generalIcons.Count; i++)
                 _generalIcons[i].transform.SetParent(_placements[_firstCommonIndex + i], false);
         }
 
-        private int GetPlacementIndex(LiveOpsIcon icon)
+        public void AddOfferButton(Transform offerButton)
         {
-            if (_reservedPlacements.TryGetValue(icon.Name, out var index))
+            _offerButton = offerButton;
+            RefreshOfferButton();
+        }
+
+        private int PreOfferButtonIndex => GetPlacementIndex("OfferButton") - 1;
+
+        private void RefreshOfferButton()
+        {
+            var offerIndex = GetPlacementIndex("OfferButton");
+            var index = _hasIconBeforeOffer ? offerIndex : offerIndex - 1;
+            _offerButton.SetParent(_placements[index], false);
+        }
+
+        private int GetPlacementIndex(string name)
+        {
+            if (_reservedPlacements.TryGetValue(name, out var index))
                 return index;
 
             return _firstCommonIndex + _generalIcons.Count;
