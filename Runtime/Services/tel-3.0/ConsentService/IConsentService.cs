84275c82f9868422d77c7cd46993b582da3ca44a
NorthernShady <ecandentia@gmail.com>
2026-05-12 01:24:35 +0500

    Add ConsentService as peer service

---
+++ Runtime/Services/tel-3.0/ConsentService/IConsentService.cs
@@ -0,0 +1,12 @@
+using R3;
+using TapEmpire.Services;
+
+namespace TapEmpire.Experimental
+{
+    public interface IConsentService : IService
+    {
+        ReadOnlyReactiveProperty<bool> IsResolved { get; }
+        ReadOnlyReactiveProperty<bool> IsPersonalized { get; }
+        ReadOnlyReactiveProperty<bool> IsEurArea { get; }
+    }
+}
---
+++ Runtime/Services/tel-3.0/ConsentService/Remote/ConsentSettingsSerializable.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TapEmpire.Services;
+
+namespace TapEmpire.Experimental
+{
+    [Serializable]
+    public class ConsentSettingsSerializable : RemoteSerializableBase<ConsentSettings, ConsentSettingsSerializable.ConsentRemoteModel>
+    {
+        public override string TokenName => "ConsentSettings";
+
+        public class ConsentRemoteModel : IRemoteModel<ConsentSettings>
+        {
+            public bool? EnableUmp;
+            public bool? EnableAtt;
+            public bool? IsForFamily;
+            public List<string> TestDeviceHashedIds;
+
+            public void FromSettings(ConsentSettings settings)
+            {
+                EnableUmp = settings.EnableUmp;
+                EnableAtt = settings.EnableAtt;
+                IsForFamily = settings.IsForFamily;
+                TestDeviceHashedIds = settings.TestDeviceHashedIds.ToList();
+            }
+
+            public void ToSettings(ConsentSettings settings)
+            {
+                settings.EnableUmp = EnableUmp ?? settings.EnableUmp;
+                settings.EnableAtt = EnableAtt ?? settings.EnableAtt;
+                settings.IsForFamily = IsForFamily ?? settings.IsForFamily;
+                if (TestDeviceHashedIds != null)
+                {
+                    settings.TestDeviceHashedIds.Clear();
+                    settings.TestDeviceHashedIds.AddRange(TestDeviceHashedIds);
+                }
+            }
+        }
+    }
+}
