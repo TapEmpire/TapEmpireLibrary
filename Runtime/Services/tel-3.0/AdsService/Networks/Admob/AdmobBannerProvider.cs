1449632a4bc32d2ea29c5c32e387f4f84a6bb1a0
NorthernShady <ecandentia@gmail.com>
2026-05-14 17:27:33 +0500

    Add AdmobBannerProvider: IBanner impl (IsLoaded, Layout in pixels)

---
+++ Runtime/Services/tel-3.0/AdsService/Networks/Admob/AdmobBannerProvider.cs
@@ -0,0 +1,84 @@
+using System;
+using GoogleMobileAds.Api;
+using R3;
+using UnityEngine;
+
+namespace TapEmpire.Experimental
+{
+    public class AdmobBannerProvider : IBanner, IDisposable
+    {
+        public ReactiveProperty<bool> IsLoaded { get; } = new(false);
+        public ReactiveProperty<Vector2> Layout { get; } = new(Vector2.zero);
+        public Subject<AdImpressionData> OnImpression { get; } = new();
+
+        private readonly string _adUnitId;
+        private readonly BannerView _bannerView;
+
+        public AdmobBannerProvider(string adUnitId, AdPosition position, int width = 0)
+        {
+            _adUnitId = adUnitId;
+
+            int bannerWidth = width > 0 ? width : AdSize.FullWidth;
+            var adSize = new AdSize(bannerWidth, 0);
+
+            _bannerView = new BannerView(_adUnitId, adSize, position.ToAdmobPosition());
+
+            _bannerView.OnBannerAdLoaded += OnAdLoaded;
+            _bannerView.OnBannerAdLoadFailed += OnAdLoadFailed;
+            _bannerView.OnAdPaid += OnAdRevenuePaid;
+
+            _bannerView.LoadAd(new AdRequest());
+        }
+
+        public void Show()
+        {
+            _bannerView.Show();
+            UpdateLayout();
+        }
+
+        public void Hide()
+        {
+            _bannerView.Hide();
+        }
+
+        public void Dispose()
+        {
+            _bannerView.OnBannerAdLoaded -= OnAdLoaded;
+            _bannerView.OnBannerAdLoadFailed -= OnAdLoadFailed;
+            _bannerView.OnAdPaid -= OnAdRevenuePaid;
+
+            _bannerView.Destroy();
+            IsLoaded.Value = false;
+        }
+
+        private void OnAdLoaded()
+        {
+            IsLoaded.Value = true;
+            UpdateLayout();
+        }
+
+        private void OnAdLoadFailed(LoadAdError _)
+        {
+            IsLoaded.Value = false;
+        }
+
+        private void OnAdRevenuePaid(AdValue value)
+        {
+            OnImpression.OnNext(new AdImpressionData(
+                AdNetwork.Admob,
+                "AdMob",
+                _adUnitId,
+                string.Empty,
+                value.Value / 1_000_000d,
+                value.CurrencyCode,
+                value.Precision.ToString()));
+        }
+
+        private void UpdateLayout()
+        {
+            float w = _bannerView.GetWidthInPixels();
+            float h = _bannerView.GetHeightInPixels();
+            Layout.Value = new Vector2(w, h);
+        }
+    }
+}
