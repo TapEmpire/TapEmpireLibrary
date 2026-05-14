fc0a200fbf22b4cce3512effb5165d9ab399d131
NorthernShady <ecandentia@gmail.com>
2026-05-14 13:24:51 +0500

    Add MaxRewardedProvider

---
+++ Runtime/Services/tel-3.0/AdsService/Networks/AppLovin/MaxRewardedProvider.cs
@@ -0,0 +1,100 @@
+using System;
+using R3;
+using TapEmpire.Utility;
+
+namespace TapEmpire.Experimental
+{
+    public class MaxRewardedProvider : IRewarded, IDisposable
+    {
+        public Subject<AdImpressionData> OnImpression { get; } = new();
+        public Subject<Unit> OnReward { get; } = new();
+
+        private readonly string _adUnitId;
+
+        private CancellableTask _retryDisposable;
+        private int _retryAttempt;
+
+        public MaxRewardedProvider(string adUnitId)
+        {
+            _adUnitId = adUnitId;
+
+            MaxSdkCallbacks.Rewarded.OnAdLoadedEvent += OnAdLoaded;
+            MaxSdkCallbacks.Rewarded.OnAdLoadFailedEvent += OnAdLoadFailed;
+            MaxSdkCallbacks.Rewarded.OnAdDisplayFailedEvent += OnAdDisplayFailed;
+            MaxSdkCallbacks.Rewarded.OnAdHiddenEvent += OnAdHidden;
+            MaxSdkCallbacks.Rewarded.OnAdReceivedRewardEvent += OnAdReceivedReward;
+            MaxSdkCallbacks.Rewarded.OnAdRevenuePaidEvent += OnAdRevenuePaid;
+
+            LoadRewardedAd();
+        }
+
+        public bool HasRewarded(bool doRequest = false)
+        {
+            bool isReady = MaxSdk.IsRewardedAdReady(_adUnitId);
+            if (!isReady && doRequest)
+            {
+                LoadRewardedAd();
+            }
+            return isReady;
+        }
+
+        public void Show(string placement)
+        {
+            MaxSdk.ShowRewardedAd(_adUnitId, placement);
+        }
+
+        public void Dispose()
+        {
+            MaxSdkCallbacks.Rewarded.OnAdLoadedEvent -= OnAdLoaded;
+            MaxSdkCallbacks.Rewarded.OnAdLoadFailedEvent -= OnAdLoadFailed;
+            MaxSdkCallbacks.Rewarded.OnAdDisplayFailedEvent -= OnAdDisplayFailed;
+            MaxSdkCallbacks.Rewarded.OnAdHiddenEvent -= OnAdHidden;
+            MaxSdkCallbacks.Rewarded.OnAdReceivedRewardEvent -= OnAdReceivedReward;
+            MaxSdkCallbacks.Rewarded.OnAdRevenuePaidEvent -= OnAdRevenuePaid;
+
+            _retryDisposable?.Dispose();
+        }
+
+        private void LoadRewardedAd() => MaxSdk.LoadRewardedAd(_adUnitId);
+
+        private void OnAdLoaded(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        {
+            _retryAttempt = 0;
+        }
+
+        private void OnAdLoadFailed(string adUnitId, MaxSdkBase.ErrorInfo errorInfo)
+        {
+            _retryAttempt++;
+            var seconds = (float)Math.Pow(2, Math.Min(6, _retryAttempt));
+            _retryDisposable?.Dispose();
+            _retryDisposable = UniTaskUtility.Delay(seconds, LoadRewardedAd);
+        }
+
+        private void OnAdDisplayFailed(string adUnitId, MaxSdkBase.ErrorInfo errorInfo, MaxSdkBase.AdInfo adInfo)
+        {
+            LoadRewardedAd();
+        }
+
+        private void OnAdHidden(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        {
+            LoadRewardedAd();
+        }
+
+        private void OnAdReceivedReward(string adUnitId, MaxSdkBase.Reward reward, MaxSdkBase.AdInfo adInfo)
+        {
+            OnReward.OnNext(Unit.Default);
+        }
+
+        private void OnAdRevenuePaid(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        {
+            OnImpression.OnNext(new AdImpressionData(
+                AdNetwork.Max,
+                adInfo.NetworkName,
+                adUnitId,
+                adInfo.Placement,
+                adInfo.Revenue,
+                "USD",
+                adInfo.RevenuePrecision));
+        }
+    }
+}
