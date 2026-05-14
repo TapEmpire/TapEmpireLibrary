7b7be86efe8091597b8423d7371d4f5a8feb23b3
NorthernShady <ecandentia@gmail.com>
2026-05-14 17:27:33 +0500

    Add AdmobInitializer: async AdMob SDK init with IsInitialized R3 prop

---
+++ Runtime/Services/tel-3.0/AdsService/Networks/Admob/AdmobInitializer.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using GoogleMobileAds.Api;
+using R3;
+
+namespace TapEmpire.Experimental
+{
+    public class AdmobInitializer
+    {
+        private readonly ReactiveProperty<bool> _isInitialized = new(false);
+
+        public ReadOnlyReactiveProperty<bool> IsInitialized => _isInitialized;
+
+        public async UniTask Initialize(
+            bool isPersonalized,
+            bool testMode = false,
+            CancellationToken cancellationToken = default)
+        {
+            if (_isInitialized.Value)
+            {
+                return;
+            }
+
+            MobileAds.SetiOSAppPauseOnBackground(true);
+            MobileAds.RaiseAdEventsOnUnityMainThread = true;
+
+            var configuration = new RequestConfiguration
+            {
+                PublisherPrivacyPersonalizationState = isPersonalized
+                    ? PublisherPrivacyPersonalizationState.Enabled
+                    : PublisherPrivacyPersonalizationState.Disabled,
+            };
+
+            if (testMode)
+            {
+                configuration.TestDeviceIds = await GetTestDeviceIds(cancellationToken);
+            }
+
+            MobileAds.SetRequestConfiguration(configuration);
+
+            var completion = new UniTaskCompletionSource();
+            MobileAds.Initialize(_ => completion.TrySetResult());
+
+            await completion.Task.AttachExternalCancellation(cancellationToken);
+            _isInitialized.Value = true;
+        }
+
+        private static async UniTask<List<string>> GetTestDeviceIds(CancellationToken cancellationToken)
+        {
+            var gaid = await AdvertisingId.Get(cancellationToken);
+            return string.IsNullOrEmpty(gaid)
+                ? new List<string>()
+                : new List<string> { gaid };
+        }
+    }
+}
