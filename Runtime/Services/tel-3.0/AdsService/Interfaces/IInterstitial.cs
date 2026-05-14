e233a93c570fd8875ab909a2b785ecde1c7dd1d2
NorthernShady <ecandentia@gmail.com>
2026-05-14 12:14:42 +0500

    Add MaxInterstitialProvider + slim IInterstitial to match

--- Runtime/Services/tel-3.0/AdsService/Interfaces/IInterstitial.cs
+++ Runtime/Services/tel-3.0/AdsService/Interfaces/IInterstitial.cs
@@ -1,17 +1,13 @@
-using System.Threading;
-using Cysharp.Threading.Tasks;
 using R3;
 
 namespace TapEmpire.Experimental
 {
     public interface IInterstitial
     {
-        ReadOnlyReactiveProperty<bool> IsLoaded { get; }
-
         Observable<AdImpressionData> OnImpression { get; }
-        Observable<AdError> OnFailed { get; }
-        Observable<Unit> OnClosed { get; }
 
-        UniTask<bool> ShowAsync(string placement, CancellationToken cancellationToken = default);
+        bool HasInterstitial(bool doRequest = false);
+
+        void Show(string placement);
     }
 }
---
+++ Runtime/Services/tel-3.0/AdsService/Networks/AppLovin/MaxInterstitialProvider.cs
@@ -0,0 +1,93 @@
+using System;
+using R3;
+using TapEmpire.Utility;
+
+namespace TapEmpire.Experimental
+{
+    public class MaxInterstitialProvider : IInterstitial, IDisposable
+    {
+        public Observable<AdImpressionData> OnImpression => _onImpression;
+
+        private readonly Subject<AdImpressionData> _onImpression = new();
+        private readonly string _adUnitId;
+
+        private CancellableTask _retryDisposable;
+        private int _retryAttempt;
+
+        public MaxInterstitialProvider(string adUnitId)
+        {
+            _adUnitId = adUnitId;
+
+            MaxSdkCallbacks.Interstitial.OnAdLoadedEvent += OnAdLoaded;
+            MaxSdkCallbacks.Interstitial.OnAdLoadFailedEvent += OnAdLoadFailed;
+            MaxSdkCallbacks.Interstitial.OnAdDisplayFailedEvent += OnAdDisplayFailed;
+            MaxSdkCallbacks.Interstitial.OnAdHiddenEvent += OnAdHidden;
+            MaxSdkCallbacks.Interstitial.OnAdRevenuePaidEvent += OnAdRevenuePaid;
+
+            LoadInterstitial();
+        }
+
+        public bool HasInterstitial(bool doRequest = false)
+        {
+            bool isReady = MaxSdk.IsInterstitialReady(_adUnitId);
+            if (!isReady && doRequest)
+            {
+                LoadInterstitial();
+            }
+            return isReady;
+        }
+
+        public void Show(string placement)
+        {
+            MaxSdk.ShowInterstitial(_adUnitId, placement);
+        }
+
+        public void Dispose()
+        {
+            MaxSdkCallbacks.Interstitial.OnAdLoadedEvent -= OnAdLoaded;
+            MaxSdkCallbacks.Interstitial.OnAdLoadFailedEvent -= OnAdLoadFailed;
+            MaxSdkCallbacks.Interstitial.OnAdDisplayFailedEvent -= OnAdDisplayFailed;
+            MaxSdkCallbacks.Interstitial.OnAdHiddenEvent -= OnAdHidden;
+            MaxSdkCallbacks.Interstitial.OnAdRevenuePaidEvent -= OnAdRevenuePaid;
+
+            _retryDisposable?.Dispose();
+        }
+
+        private void LoadInterstitial() => MaxSdk.LoadInterstitial(_adUnitId);
+
+        private void OnAdLoaded(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        {
+            _retryAttempt = 0;
+        }
+
+        private void OnAdLoadFailed(string adUnitId, MaxSdkBase.ErrorInfo errorInfo)
+        {
+            _retryAttempt++;
+            var seconds = (float)Math.Pow(2, Math.Min(6, _retryAttempt));
+            _retryDisposable?.Dispose();
+            _retryDisposable = UniTaskUtility.Delay(seconds, LoadInterstitial);
+        }
+
+        private void OnAdDisplayFailed(string adUnitId, MaxSdkBase.ErrorInfo errorInfo, MaxSdkBase.AdInfo adInfo)
+        {
+            LoadInterstitial();
+        }
+
+        private void OnAdHidden(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        {
+            LoadInterstitial();
+        }
+
+        private void OnAdRevenuePaid(string adUnitId, MaxSdkBase.AdInfo adInfo)
+        {
+            _onImpression.OnNext(new AdImpressionData(
+                AdNetwork.Max,
+                adInfo.NetworkName,
+                adUnitId,
+                adInfo.Placement,
+                adInfo.Revenue,
+                "USD",
+                adInfo.RevenuePrecision));
+        }
+    }
+}
