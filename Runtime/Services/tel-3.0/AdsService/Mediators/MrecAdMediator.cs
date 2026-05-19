191ce649f03043deece1bf3d1be16b9e0a720a8a
NorthernShady <ecandentia@gmail.com>
2026-05-19 21:08:14 +0400

    MrecAdMediator: align with Banner pattern (Refresh+Apply+ShowInternal, shouldShow flag, Vector2Int? position)

--- Runtime/Services/tel-3.0/AdsService/Mediators/MrecAdMediator.cs
+++ Runtime/Services/tel-3.0/AdsService/Mediators/MrecAdMediator.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using R3;
+using UnityEngine;
 
 namespace TapEmpire.Experimental
 {
@@ -12,7 +14,8 @@ namespace TapEmpire.Experimental
         private readonly IReadOnlyList<IMrec> _providers;
         private readonly CompositeDisposable _disposables = new();
 
-        private IMrec _activeProvider;
+        private bool _shouldShow;
+        private Vector2Int? _customPosition;
 
         public MrecAdMediator(IReadOnlyList<IMrec> providers)
         {
@@ -22,50 +25,59 @@ namespace TapEmpire.Experimental
             {
                 provider.OnImpression.Subscribe(OnImpression.OnNext).AddTo(_disposables);
                 provider.IsLoaded
-                    .Where(_ => provider == _activeProvider)
-                    .Subscribe(loaded => IsLoaded.Value = loaded)
+                    .Subscribe(_ => IsLoaded.Value = _providers.Any(provider => provider.IsLoaded.Value))
                     .AddTo(_disposables);
             }
+
+            _providers[0].IsLoaded.Subscribe(Refresh).AddTo(_disposables);
         }
 
         public void Show()
         {
-            ShowOnPreferred(provider => provider.Show());
+            _shouldShow = true;
+            _customPosition = null;
+            _providers.FirstOrDefault(provider => provider.IsLoaded.Value)?.Show();
         }
 
         public void Show(int x, int y)
         {
-            ShowOnPreferred(provider => provider.Show(x, y));
+            _shouldShow = true;
+            _customPosition = new Vector2Int(x, y);
+            _providers.FirstOrDefault(provider => provider.IsLoaded.Value)?.Show(x, y);
         }
 
         public void Hide()
         {
+            _shouldShow = false;
             foreach (var provider in _providers) provider.Hide();
-            _activeProvider = null;
-            IsLoaded.Value = false;
         }
 
-        private void ShowOnPreferred(Action<IMrec> show)
+        private void Refresh(bool isLoaded)
         {
-            IMrec preferred = null;
-            foreach (var provider in _providers)
+            if (isLoaded)
             {
-                if (provider.IsLoaded.Value) { preferred = provider; break; }
+                foreach (var provider in _providers.Skip(1)) provider.Hide();
+                Apply(_providers[0]);
             }
-            preferred ??= _providers[0];
-
-            foreach (var provider in _providers)
+            else
             {
-                if (provider != preferred) provider.Hide();
+                _providers[0].Hide();
+                Apply(_providers.Skip(1).FirstOrDefault(provider => provider.IsLoaded.Value));
             }
-            show(preferred);
-            _activeProvider = preferred;
-            IsLoaded.Value = preferred.IsLoaded.Value;
         }
 
-        public void Dispose()
+        private void Apply(IMrec provider)
         {
-            _disposables.Dispose();
+            if (_shouldShow) ShowInternal(provider);
+            else provider.Hide();
         }
+
+        private void ShowInternal(IMrec provider)
+        {
+            if (_customPosition.HasValue) provider.Show(_customPosition.Value.x, _customPosition.Value.y);
+            else provider.Show();
+        }
+
+        public void Dispose() => _disposables.Dispose();
     }
 }
