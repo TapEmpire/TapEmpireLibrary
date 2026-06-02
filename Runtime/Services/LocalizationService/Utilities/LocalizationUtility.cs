01c809d0afd37511fc899b518cfb7cae01295009
NorthernShady <ecandentia@gmail.com>
2026-06-02 22:29:38 +0400

    ~ StringTable.CreateOrUpdateEntry extension, deduplicate GoogleSheetData

--- Runtime/Services/LocalizationService/Utilities/LocalizationUtility.cs
+++ Runtime/Services/LocalizationService/Utilities/LocalizationUtility.cs
@@ -1,4 +1,5 @@
 using UnityEngine.Localization;
+using UnityEngine.Localization.Tables;
 
 namespace TapEmpire.Services.Localization
 {
@@ -7,6 +8,19 @@ namespace TapEmpire.Services.Localization
         public static string GetCountryCode(this LocaleIdentifier identifier)
         {
             return identifier.Code.Split('-')[0];
+        }
+
+        public static void CreateOrUpdateEntry(this StringTable table, string entryName, string localizedString)
+        {
+            var entry = table.GetEntry(entryName);
+            if (entry != null)
+            {
+                entry.Value = localizedString;
+            }
+            else
+            {
+                table.AddEntry(entryName, localizedString);
+            }
         }
     }
 }
