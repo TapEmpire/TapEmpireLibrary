c2f75ee45d89036789b474e3ba8997b082f968cf
NorthernShady <ecandentia@gmail.com>
2026-07-28 00:19:46 +0400

    Add ILevelExecutionCoreSystem and its extensions, typed on TEL LevelSettings

---
+++ Runtime/CoreSystems/LevelExecutionCoreSystem/ILevelExecutionCoreSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using R3;
+using TapEmpire.Level;
+
+namespace TapEmpire.CoreSystems
+{
+    public interface ILevelExecutionCoreSystem : IExecutionCoreSystem
+    {
+        ReactiveProperty<LevelExecutionData> ExecutionData { get; }
+        IReadOnlyList<LevelSettings> Levels { get; }
+
+        Subject<LevelExecutionData> OnLevelStartedR3 { get; }
+        Subject<LevelEndReason> OnLevelCompletedR3 { get; }
+
+        event Action<LevelExecutionData> OnLevelStarted;
+        event Action<LevelEndReason> OnLevelCompleted;
+        event Action<int> OnCycleCompleted;
+
+        void StartLevel(int levelIndex);
+        void Continue();
+        void ExitLevel(LevelEndReason reason);
+        void RestartLevel(bool isDebug = false);
+        void SetShouldSkipAd(bool shouldSkip);
+    }
+}
---
+++ Runtime/CoreSystems/LevelExecutionCoreSystem/LevelExecutionCoreSystemExtensions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using TapEmpire.Utility;
+
+namespace TapEmpire.CoreSystems
+{
+    public static class LevelExecutionCoreSystemExtensions
+    {
+        public static bool TryGetCamera(this ILevelExecutionCoreSystem self, out Camera camera)
+        {
+            if (self.ExecutionData.Value == null || self.ExecutionData.Value.Camera == null)
+            {
+                camera = null;
+                return false;
+            }
+            camera = self.ExecutionData.Value.Camera;
+            return true;
+        }
+
+        public static int GetLevelIndex(this ILevelExecutionCoreSystem self)
+        {
+            return self.ExecutionData.Value.LevelIndex;
+        }
+
+        public static int GetNextLevelIndex(this ILevelExecutionCoreSystem self)
+        {
+            var levels = self.Levels;
+            var currentLevelIndex = self.GetLevelIndex();
+
+            return MathUtility.LoopClamp(currentLevelIndex + 1, levels.Count);
+        }
+
+        public static void StartNextLevel(this ILevelExecutionCoreSystem self)
+        {
+            self.StartLevel(self.GetNextLevelIndex());
+        }
+    }
+}
